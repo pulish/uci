a6267016284380d2894edb61df27cc2c046f8edc
Peter Bulisch <56633213+pulish@users.noreply.github.com>
2022-02-01 20:07:00 +0100

    delete old tags, add subbuild

--- create_tag.sh
+++ create_tag.sh
@@ -3,21 +3,32 @@
 set -e
 
 # get latest version link of 8 numeric chars beginning with 20
-BUILD=$(curl -s https://cloud-images.ubuntu.com/focal/ | grep 'href="20' | sed 's/.*href="\([0-9]\{8\}\).*/\1/g' | sort -nr | head -n 1)
-
-if echo "$BUILD" | grep -q '^[0-9]\{8\}$'; then
-    echo "INFO: use build ${BUILD}"
-else
-    echo "ERROR: found invalid build: ${BUILD} under url https://cloud-images.ubuntu.com/focal/"
-    exit 1
-fi
+BUILDS=$(curl -s https://cloud-images.ubuntu.com/focal/ | grep 'href="20' | sed 's/.*href="\([\.0-9]\{8,10\}\).*/\1/g' | sort -V)
 
 TAGS=$(git tag)
-if echo "${TAGS}" | grep -q "${BUILD}"; then
-    echo "INFO: build ${BUILD} already in tag list"
-    exit 0
-fi
 
-git tag "${BUILD}"
-git push origin "${BUILD}"
-echo "INFO: added ${BUILD} to tag list"
+for BUILD in ${BUILDS}; do
+    if ! echo "$BUILD" | grep -q '^[\.0-9]\{8,10\}$'; then
+        echo "ERROR: found invalid build: ${BUILD} under url https://cloud-images.ubuntu.com/focal/"
+        exit 1
+    fi
+
+    if echo "${TAGS}" | grep -q "${BUILD}"; then
+        echo "INFO: build ${BUILD} already in tag list"
+        continue
+    fi
+
+    git tag "${BUILD}"
+    git push origin "${BUILD}"
+    echo "INFO: added ${BUILD} to tag list"
+done
+
+for TAG in ${TAGS}; do
+    if echo "${BUILDS}" | grep -q "${TAG}"; then
+        continue
+    fi
+
+    git tag -d "${TAG}"
+    git push --delete origin "${TAG}"
+    echo "INFO: delete ${TAG} from tag list"
+done
